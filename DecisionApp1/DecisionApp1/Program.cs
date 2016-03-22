737f246db4f62603320c9248f4fdb710894d1a6c
Ken Wagner <kenwagner@hotmail.com>
2016-03-22 14:52:35 -0700

    Organizing the files
    Reorganizing the files and repositories on github.

---
+++ DecisionApp1/DecisionApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt user for minutes exercised
+            Console.Write("Enter the minutes exercised: ");
+
+            string entry = Console.ReadLine();
+
+            //Console.Write(entry);
+
+
+            //Add minutes exercised to total
+            //Display total minutes exercised to screen
+
+            Console.WriteLine("You've entered " + entry + " minutes");
+            //Repeat until user quits
+        }
+    }
+}
